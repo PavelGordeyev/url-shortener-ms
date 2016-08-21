c3b46086d4a07e9ffa25596748e8b87f987bb3d3
Pavel Gordeyev <gordeyev.p@gmail.com>
2016-08-21 19:38:14 -0400

    Add connection to MongoLab and add error.jade

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const hostname = parseInt(process.env.PORT, 10) ? '0.0.0.0' : '127.0.0.1';
 // Database url
 // var url = 'mongodb://localhost:27017/nodetest1';
 
-var db = monk('localhost:27017/shortURLs');
+var db = monk('example:example123@ds013486.mlab.com:13486/shorturls');
 
 // Set public directory
 app.use('/static', express.static(__dirname + './public'));
@@ -36,37 +36,22 @@ app.use(function(req,res,next){
 
 app.use('/',routes);
 
-/// catch 404 and forwarding to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
+// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-// /// error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 // Listen on port
 app.listen(port,hostname,function(){
